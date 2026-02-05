5d6db370d3553896810850d089fba9351a7260b8
Anbarasan <radnustech.team2026@gmail.com>
2026-02-05 17:44:20 +0530

    Updated firebase service dev by anbu

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,5 +1,21 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../firebase-services.json")
+
+let serviceAccount;
+
+switch (process.env.NODE_ENV) {
+
+  case "production":
+    serviceAccount = require("../firebase-services.json");
+    break;
+
+  case "development":
+    serviceAccount = require("../firebase-services-dev.json");
+    break;
+
+  default:
+    console.warn("NODE_ENV not set. Using development Firebase config.");
+    serviceAccount = require("../firebase-services-dev.json");
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
