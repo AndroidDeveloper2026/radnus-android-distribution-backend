d26dee946dcf2d513fd0aebcb59b27e74db6ce8c
Anbarasan <radnustech.team2026@gmail.com>
2026-02-17 16:31:01 +0530

    update firebaseAdmin file

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,24 +1,39 @@
 const admin = require("firebase-admin");
 
-let serviceAccount;
-
-switch (process.env.NODE_ENV) {
-
-  case "prod":
-    serviceAccount = require("../firebase-services-prod.json");
-    break;
-
-  case "dev":
-    serviceAccount = require("../firebase-services-dev.json");
-    break;
-
-  default:
-    console.warn("NODE_ENV not set. Using development Firebase config.");
-    serviceAccount = require("../firebase-services-dev.json");
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+  });
 }
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
 module.exports = admin;
+
+
+// const admin = require("firebase-admin");
+
+// let serviceAccount;
+
+// switch (process.env.NODE_ENV) {
+
+//   case "prod":
+//     serviceAccount = require("../firebase-services-prod.json");
+//     break;
+
+//   case "dev":
+//     serviceAccount = require("../firebase-services-dev.json");
+//     break;
+
+//   default:
+//     console.warn("NODE_ENV not set. Using development Firebase config.");
+//     serviceAccount = require("../firebase-services-dev.json");
+// }
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
+
+// module.exports = admin;
