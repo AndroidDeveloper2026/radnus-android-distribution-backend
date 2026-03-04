1e8f27eb57267152f8f8b0f14a72be64b2475ed0
Anbarasan <radnustech.team2026@gmail.com>
2026-03-04 11:48:02 +0530

    Update session for location track

--- models/FSEModel/Session.js
+++ models/FSEModel/Session.js
@@ -1,3 +1,34 @@
+// const mongoose = require("mongoose");
+
+// const sessionSchema = new mongoose.Schema({
+//  userId: String,
+
+//  startLocation: {
+//    latitude: Number,
+//    longitude: Number,
+//  },
+
+//  route: [
+//    {
+//      latitude: Number,
+//      longitude: Number,
+//      timestamp: { type: Date, default: Date.now },
+//    },
+//  ],
+
+//  startTime: { type: Date, default: Date.now },
+//  endTime: Date,
+
+//  status: {
+//    type: String,
+//    enum: ["ACTIVE", "ENDED", "AUTO_ENDED"],
+//    default: "ACTIVE",
+//  },
+// });
+
+// module.exports = mongoose.model("Session", sessionSchema);
+
+
 const mongoose = require("mongoose");
 
 const sessionSchema = new mongoose.Schema({
@@ -16,12 +47,16 @@ const sessionSchema = new mongoose.Schema({
    },
  ],
 
- startTime: { type: Date, default: Date.now },
+ startTime: {
+   type: Date,
+   default: Date.now,
+ },
+
  endTime: Date,
 
  status: {
    type: String,
-   enum: ["ACTIVE", "ENDED", "AUTO_ENDED"],
+   enum: ["ACTIVE", "ENDED"],
    default: "ACTIVE",
  },
 });
