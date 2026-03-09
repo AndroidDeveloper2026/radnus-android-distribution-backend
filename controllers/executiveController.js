53d734a899afe9f6cf7e7093b5eec93aa24ed28a
Anbarasan <radnustech.team2026@gmail.com>
2026-03-09 10:38:25 +0530

    Updated Executive controllers

--- controllers/executiveController.js
+++ controllers/executiveController.js
@@ -1,12 +1,82 @@
+// const Executive = require("../models/Executive/Executive");
+
+// /* CREATE EXECUTIVE */
+
+// exports.createExecutive = async (req, res) => {
+//   try {
+//     const photo = req.file
+//       ? `http://localhost:5000/uploads/${req.file.filename}`
+//       : "";
+
+//     const executive = new Executive({
+//       ...req.body,
+//       photo,
+//     });
+
+//     await executive.save();
+
+//     res.json(executive);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };
+
+// /* GET ALL EXECUTIVES */
+
+// exports.getExecutives = async (req, res) => {
+//   try {
+//     const executives = await Executive.find().sort({ createdAt: -1 });
+
+//     res.json(executives);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };
+
+// /* UPDATE EXECUTIVE */
+
+// exports.updateExecutive = async (req, res) => {
+//   try {
+//     const executive = await Executive.findByIdAndUpdate(
+//       req.params.id,
+//       req.body,
+//       { new: true },
+//     );
+
+//     res.json(executive);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };
+
+// /* DELETE EXECUTIVE */
+
+// exports.deleteExecutive = async (req, res) => {
+//   try {
+//     await Executive.findByIdAndDelete(req.params.id);
+
+//     res.json({ message: "Executive deleted" });
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };
+
 const Executive = require("../models/Executive/Executive");
+const cloudinary = require("cloudinary").v2;
 
 /* CREATE EXECUTIVE */
 
 exports.createExecutive = async (req, res) => {
   try {
-    const photo = req.file
-      ? `http://localhost:5000/uploads/${req.file.filename}`
-      : "";
+    let photo = "";
+
+    if (req.file) {
+      const result = await cloudinary.uploader.upload(req.file.path, {
+        folder: "executives",
+      });
+
+      photo = result.secure_url;
+    }
 
     const executive = new Executive({
       ...req.body,
@@ -21,6 +91,7 @@ exports.createExecutive = async (req, res) => {
   }
 };
 
+
 /* GET ALL EXECUTIVES */
 
 exports.getExecutives = async (req, res) => {
@@ -33,14 +104,25 @@ exports.getExecutives = async (req, res) => {
   }
 };
 
+
 /* UPDATE EXECUTIVE */
 
 exports.updateExecutive = async (req, res) => {
   try {
+    let updateData = { ...req.body };
+
+    if (req.file) {
+      const result = await cloudinary.uploader.upload(req.file.path, {
+        folder: "executives",
+      });
+
+      updateData.photo = result.secure_url;
+    }
+
     const executive = await Executive.findByIdAndUpdate(
       req.params.id,
-      req.body,
-      { new: true },
+      updateData,
+      { new: true }
     );
 
     res.json(executive);
@@ -48,6 +130,7 @@ exports.updateExecutive = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
 
 /* DELETE EXECUTIVE */
 
