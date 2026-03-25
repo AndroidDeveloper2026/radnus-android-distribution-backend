6be8d65c5409a077fdc843c0977fd327cabe691f
Anbarasan <radnustech.team2026@gmail.com>
2026-03-25 11:07:08 +0530

    update code of models/RetailerModel/Retailer.js

--- models/RetailerModel/Retailer.js
+++ models/RetailerModel/Retailer.js
@@ -1,3 +1,24 @@
+// const mongoose = require("mongoose");
+
+// const retailerSchema = new mongoose.Schema(
+//   {
+//     shopName: String,
+//     ownerName: String,
+//     mobile: String,
+//     gps: String,
+//     shopPhoto: String,
+
+//     status: {
+//       type: String,
+//       enum: ["PENDING", "APPROVED", "REJECTED"],
+//       default: "PENDING",
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// module.exports = mongoose.model("Retailer", retailerSchema);
+
 const mongoose = require("mongoose");
 
 const retailerSchema = new mongoose.Schema(
@@ -5,7 +26,10 @@ const retailerSchema = new mongoose.Schema(
     shopName: String,
     ownerName: String,
     mobile: String,
-    gps: String,
+
+    area: String,   
+    gst: String,    
+
     shopPhoto: String,
 
     status: {
