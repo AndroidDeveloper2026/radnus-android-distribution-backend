5cc13a4f5691c1c38c57c90963cd0d8a24b15777
Anbarasan <radnustech.team2026@gmail.com>
2026-04-01 17:12:43 +0530

    Update controllers/authController.js code

--- controllers/authController.js
+++ controllers/authController.js
@@ -555,7 +555,7 @@ exports.adminLogin = async (req, res) => {
 
 exports.login = async (req, res) => {
  try {
-   const user = await User.findOne({ email: req.body.email });
+   const user = await Register.findOne({ email: req.body.email });
 
    if (!user) return res.status(400).json({ message: "User not found" });
 
