24b5e37fd00eddb5b7a978830891844e220dafbe
webdevelopmentdept-dotcom <Webdevloepement@gmail.com>
2026-04-21 16:38:56 +0530

    Update ActivityLog model

--- models/ActivityLog.js
+++ models/ActivityLog.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 const activityLogSchema = new mongoose.Schema({
-  action: { type: String, enum: ['ADD_PRODUCT', 'EDIT_PRODUCT'], required: true },
+  action: { type: String, enum: ['ADD_PRODUCT', 'EDIT_PRODUCT','EDIT_CUSTOMER'], required: true },
   productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
   productName: { type: String },
   user: { type: String, required: true },        // login user's name
