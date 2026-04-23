338c78ea40735448c266eaca4c875c5048ee2c9c
webdevelopmentdept-dotcom <Webdevloepement@gmail.com>
2026-04-23 11:42:56 +0530

    Update code of models/AdminModel/Product.js

--- models/AdminModel/Product.js
+++ models/AdminModel/Product.js
@@ -1,3 +1,30 @@
+// const mongoose = require("mongoose");
+
+// const productSchema = new mongoose.Schema(
+//   {
+//     name: { type: String, required: true },
+//     sku: { type: String, required: true, unique: true },
+//     category: { type: String, required: true },
+
+//     mrp: { type: Number, required: true },
+//     distributorPrice: { type: Number, required: true },
+//     retailerPrice: { type: Number, required: true },
+//     itemCost: { type: Number, required: true },
+
+//     gst: { type: Number, default: 0 },
+//     moq: { type: Number, default: 1 },
+
+//     image: { type: String },
+//     status: { type: String, default: "Active" },
+//     walkinPrice: { type: Number, required: true },
+//     batchNo: { type: String, default: "" }, // ✅ NEW
+//     rackNo: { type: String, default: "" }, // ✅ NEW
+//   },
+//   { timestamps: true },
+// );
+
+// module.exports = mongoose.model("Product", productSchema);
+
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema(
@@ -17,10 +44,11 @@ const productSchema = new mongoose.Schema(
     image: { type: String },
     status: { type: String, default: "Active" },
     walkinPrice: { type: Number, required: true },
-    batchNo: { type: String, default: "" }, // ✅ NEW
-    rackNo: { type: String, default: "" }, // ✅ NEW
+    batchNo: { type: String, default: "" },
+    rackNo: { type: String, default: "" },
+    vendorName: { type: String, default: "" }   // ✅ NEW
   },
-  { timestamps: true },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Product", productSchema);
