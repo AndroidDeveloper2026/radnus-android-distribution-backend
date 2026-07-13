4fa1045063a6964f451085afbaf58cd643cf1b97
webdevelopmentdept-dotcom <Webdevloepement@gmail.com>
2026-07-13 11:58:02 +0530

    Purchase Price History screen

--- routes/purchaseRoutes.js
+++ routes/purchaseRoutes.js
@@ -1,3 +1,4 @@
+// routes/purchaseRoutes.js
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/authMiddleware");
@@ -10,23 +11,24 @@ const {
   getStockAging,
   getNonMovingStock,
   getPriceHistory,
+  getProductPriceHistory, // NEW
 } = require("../controllers/purchaseController");
 
 router.post("/", auth, createPurchaseEntry);
 router.get("/", auth, getPurchaseEntries);
 
-// Report routes must be declared before "/:id" or Express will treat
-// "stock-aging" / "non-moving-stock" as an :id param.
+// Report routes
 router.get("/stock-aging", auth, getStockAging);
 router.get("/non-moving-stock", auth, getNonMovingStock);
 router.get("/price-history/:productId", auth, getPriceHistory);
+router.get("/product-price-history/:productId", auth, getProductPriceHistory); // NEW
 
 router.get("/:id", auth, getPurchaseEntryById);
 router.put("/:id", auth, updatePurchaseEntry);
 
 module.exports = router;
 
-
+//++++++++++++++++++++++++++++++++++++++++++++++++
 // const express = require("express");
 // const router = express.Router();
 // const auth = require("../middleware/authMiddleware");
@@ -38,6 +40,7 @@ module.exports = router;
 //   updatePurchaseEntry,
 //   getStockAging,
 //   getNonMovingStock,
+//   getPriceHistory,
 // } = require("../controllers/purchaseController");
 
 // router.post("/", auth, createPurchaseEntry);
@@ -47,6 +50,7 @@ module.exports = router;
 // // "stock-aging" / "non-moving-stock" as an :id param.
 // router.get("/stock-aging", auth, getStockAging);
 // router.get("/non-moving-stock", auth, getNonMovingStock);
+// router.get("/price-history/:productId", auth, getPriceHistory);
 
 // router.get("/:id", auth, getPurchaseEntryById);
 // router.put("/:id", auth, updatePurchaseEntry);
