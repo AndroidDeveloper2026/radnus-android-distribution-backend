28799089cf3bdd536a4ae4691b476dd3ec705fcb
webdevelopmentdept-dotcom <Webdevloepement@gmail.com>
2026-07-15 12:08:56 +0530

    Update code for createdby in purchaseEntry

--- utils/token.js
+++ utils/token.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const generateAccessToken = (user) => {
  return jwt.sign(
-   { id: user._id, role: user.role },
+   { id: user._id, role: user.role, name: user.name },
    process.env.ACCESS_SECRET,
    { expiresIn: "15m" }
  );
@@ -17,3 +17,24 @@ const generateRefreshToken = (user) => {
 };
 
 module.exports = { generateAccessToken, generateRefreshToken };
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++
+// const jwt = require("jsonwebtoken");
+
+// const generateAccessToken = (user) => {
+//  return jwt.sign(
+//    { id: user._id, role: user.role },
+//    process.env.ACCESS_SECRET,
+//    { expiresIn: "15m" }
+//  );
+// };
+
+// const generateRefreshToken = (user) => {
+//  return jwt.sign(
+//    { id: user._id },
+//    process.env.REFRESH_SECRET,
+//    { expiresIn: "7d" }
+//  );
+// };
+
+// module.exports = { generateAccessToken, generateRefreshToken };
